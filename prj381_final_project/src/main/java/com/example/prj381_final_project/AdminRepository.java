4fbe11e19ea7d88898853491ea97c3a9b66c4329
martin <martinerasmus1999@gmail.com>
2022-08-21 17:40:20 +0200

    add admin class and repo to save admin users

---
+++ prj381_final_project/src/main/java/com/example/prj381_final_project/AdminRepository.java
@@ -0,0 +1,7 @@
+package com.example.prj381_final_project;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface AdminRepository  extends JpaRepository<Admin, Long> {
+    
+}
